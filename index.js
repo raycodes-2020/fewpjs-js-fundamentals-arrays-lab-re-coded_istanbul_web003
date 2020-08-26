bd261377561b590b4b22a04bb3878c13a564251a
ray <rayissam22@gmail.com>
2020-08-26 21:03:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+let append = ["Milo", "Otis", "Garfield"];
+let prepend = ["Milo", "Otis", "Garfield"];
+let removeLast = ["Milo", "Otis", "Garfield"];
+let removeFirst = ["Milo", "Otis", "Garfield"];
+
+
+append.push("Odie");
+prepend.unshift('Odie');
+removeLast.splice(2, 1);
+removeFirst.splice(0, 1);
